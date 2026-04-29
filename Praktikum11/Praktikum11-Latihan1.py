632bc0b80dd06dc318c91ce0bd812e9fa26f824a
barufaris782-droid <barufaris782@gmail.com>
2026-04-29 18:51:06 +0700

    Implement BFS and add analysis questions
    Added BFS implementation and analysis questions.

--- Praktikum11/Praktikum11-Latihan1.py
+++ Praktikum11/Praktikum11-Latihan1.py
@@ -21,3 +21,11 @@ def bfs(graph, start):
 print()
 print("BFS dari Rumah:")
 bfs(graph, 'Rumah')
+'''Pertanyaan Analisis
+1. Node mana yang dikunjungi pertama?
+2. Mengapa BFS cocok untuk mencari jalur terdekat?
+3. Apa perbedaan urutan BFS jika struktur graph diubah?'''
+
+#1. Node yang dikunjungi pertama adalah 'Rumah', karena BFS memulai penelusuran dari node awal yang diberikan, yaitu 'Rumah'.
+#2. BFS cocok untuk mencari jalur terdekat karena BFS mengeksplorasi semua node pada level yang sama sebelum melanjutkan ke level berikutnya. Dengan cara ini, BFS akan menemukan jalur terpendek (dalam hal jumlah edge) dari node awal ke node tujuan jika ada.
+#3. Jika struktur graph diubah, urutan BFS akan berubah sesuai dengan hubungan antar node yang baru. Misalnya, jika kita menambahkan node baru yang terhubung langsung ke 'Rumah', maka node tersebut akan dikunjungi lebih awal dalam urutan BFS dibandingkan dengan node-node yang sebelumnya terhubung ke 'Rumah'. Urutan BFS sangat bergantung pada struktur graph dan hubungan antar node.
